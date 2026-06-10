08e004d49e477e6c584a6f445e253a6f88096e63
pepealania <67352950+pepealania@users.noreply.github.com>
2026-06-10 17:52:06 -0500

    Create Program class with variable initialization
    Added a new C# program that initializes variables and prints them.

---
+++ c # 1/variables/variables_03_jose.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static string springIdea = "be good"; 
+
+    static void Main()
+    {
+        double salary = 50000.50;
+        string name = "John";
+
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(salary);
+    }
+}
