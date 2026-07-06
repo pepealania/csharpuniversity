def533eda871ef0f6e0aac5c75afed7c2cff922a
pepealania <67352950+pepealania@users.noreply.github.com>
2026-07-05 20:46:10 -0500

    Update nullableValueTypes.cs

--- c#2/variables/nullableValueTypes.cs
+++ c#2/variables/nullableValueTypes.cs
@@ -1,1 +1,10 @@
+int? databaseAge = null; // Perfectly legal now!
 
+if (databaseAge.HasValue)
+{
+    Console.WriteLine("User age is: " + databaseAge.Value);
+}
+else
+{
+    Console.WriteLine("Age data is missing.");
+}
